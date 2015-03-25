0b6cdf5dec127a47cb92311d7046cdeaaa7cb689
André Erdmann <dywi@mailerd.de>
2015-03-25 18:49:02 +0100

    package/fischstaebchen: vbump to 0.1.5

--- package/fischstaebchen/fischstaebchen.mk
+++ package/fischstaebchen/fischstaebchen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FISCHSTAEBCHEN_VERSION         = 0.1.1
+FISCHSTAEBCHEN_VERSION         = 0.1.5
 FISCHSTAEBCHEN_LIVEVER_REF     = v$(FISCHSTAEBCHEN_VERSION)
 #FISCHSTAEBCHEN_LIVEVER_REF     = <sha>
 FISCHSTAEBCHEN_SOURCE          = fischstaebchen-$(FISCHSTAEBCHEN_LIVEVER_REF).tar.gz
